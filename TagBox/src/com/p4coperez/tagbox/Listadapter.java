982c5a0105b461d89a56355a17975104ab199395
p4coperez <fperez@emergya.com>
2014-08-17 13:19:00 +0200

    add effect on buttons, code for get info 'dir, file ...' and code Listadapter

---
+++ TagBox/src/com/p4coperez/tagbox/Listadapter.java
@@ -0,0 +1,115 @@
+package com.p4coperez.tagbox;
+ 
+ 
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+ 
+public class Listadapter extends BaseAdapter{
+ 
+ String[] elementsName;
+ String[] elementsText;
+ Activity context;
+ boolean[] elementsChecked;
+ 
+ public Listadapter(Activity context, String[] itemsListElemetsName,
+   String[] itemsListElemetsText, boolean[] itemsListElementsChecked) {
+  super();
+  this.context = context;
+  this.elementsName = itemsListElemetsName;
+  this.elementsText = itemsListElemetsText;
+  this.elementsChecked = itemsListElementsChecked;
+ }
+ 
+ private class ViewHolder {
+  TextView apkName;
+  CheckBox ck1;
+ }
+ 
+ public int getCount() {
+  return elementsName.length;
+ }
+ 
+ public Object getItemName(int position) {
+  return elementsName[position];
+ }
+ 
+ public boolean getItemChecked(int position) {
+	  return elementsChecked[position];
+	 }
+ 
+ public Object getItemText(int position) {
+	  return elementsText[position];
+	 }
+ 
+ public long getItemId(int position) {
+  return 0;
+ }
+ 
+ @Override
+ public Object getItem(int position) {
+  return null;
+ }
+ 
+ @Override
+ public View getView(final int position, View convertView, ViewGroup parent) {
+  final ViewHolder holder;
+   
+  LayoutInflater inflater = context.getLayoutInflater();
+ 
+  if (convertView == null) {
+   convertView = inflater.inflate(R.layout.element, null);
+   holder = new ViewHolder();
+ 
+   holder.apkName = (TextView) convertView
+     .findViewById(R.id.elementBox1);
+   holder.ck1 = (CheckBox) convertView
+     .findViewById(R.id.checkBox1);
+ 
+   convertView.setTag(holder);
+ 
+  } else {
+    
+   holder = (ViewHolder) convertView.getTag();
+  }
+ 
+ 
+ 
+  /*Drawable appIcon = packageManager
+    .getApplicationIcon(packageInfo.applicationInfo);*/
+  String appName = (String) getItemName(position);
+  boolean appChecked = (boolean) getItemChecked(position);
+  /*appIcon.setBounds(0, 0, 40, 40);
+  holder.apkName.setCompoundDrawables(appIcon, null, null, null);
+  holder.apkName.setCompoundDrawablePadding(15);*/
+  holder.apkName.setText(appName);
+  holder.ck1.setChecked(appChecked);
+ 
+  if (elementsChecked[position])
+   holder.ck1.setChecked(true);
+  else
+   holder.ck1.setChecked(false);
+ 
+  holder.ck1.setOnClickListener(new OnClickListener() {
+   @Override
+   public void onClick(View v) {
+    // TODO Auto-generated method stub
+    if (holder.ck1.isChecked())
+    	elementsChecked[position] = true;
+    else
+    	elementsChecked[position] = false;
+   }
+  });
+ 
+  return convertView;
+ 
+ }
+ 
+ 
+ 
+}
--- TagBox/src/com/p4coperez/tagbox/MainActivity.java
+++ TagBox/src/com/p4coperez/tagbox/MainActivity.java
@@ -18,9 +18,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import android.app.ListActivity;
 import android.content.Intent;
@@ -31,8 +34,14 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,39 +50,120 @@ public class MainActivity extends ListActivity {
 	private static final int STATIC_INTEGER_VALUE_CONFIG_UPDATE = 4357;
 	private static final int STATIC_INTEGER_VALUE_CONFIG_ACTUAL = 0000;
 
-	TextView tv1;
-	ImageView iv1;
-	ImageView iv2;
-
+	TextView tvPath;
+	TextView tv2;
+	ArrayList <String> checkedValue;
+	ListView listApps;
 	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		setListAdapter(null);
+		//setListAdapter(null);
+		//setListAdapter(new IconAdapter()); 
 		
-		//et1.setText(text1.getText().toString());
-		//et1.setText(text2.getText().toString());
-		
-		/*SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-		tv1 = (TextView) findViewById(R.id.textView1);
-		tv1.setText(preferences.getString("route", "/sdcard/")); 
-		*/
 		Intent p = new Intent(this, Configuration.class);
 		p.putExtra("itemSelected",false);
 		startActivityForResult(p,STATIC_INTEGER_VALUE_CONFIG_ACTUAL);
 		
+		listApps = (ListView) findViewById(android.R.id.list);
+
+		//ListView of Elements
+		tvPath = (TextView) findViewById(R.id.textViewRouteConfig);
+		File tarjeta = Environment.getExternalStorageDirectory();
+		//File dir = new File(tarjeta.getAbsolutePath()+tvPath.getText().toString());
+		File dir = new File(tarjeta.getAbsolutePath());
+		File[] filelist = dir.listFiles();
 		
+		// Count number of files to read
+		String[] itemsFileNames = new String[filelist.length];
+		int countList = 0;
+		for (int j = 0; j < filelist.length; j++) {
+			//Get the text file
+			File file = new File(tarjeta.getAbsolutePath(),itemsFileNames.toString());
+
+			if (file.isFile() && !file.isHidden()) {
+				countList = countList +1;
+			}
+		}
+		
+		
+		String[] itemsElementsNames = new String[countList];
+		String[] itemsElementsTexts = new String[countList];
+		boolean[] itemsElementsChecked = new boolean[countList];
+		
+		StringBuilder text;
+		
+		for (int i = 0; i < itemsElementsNames.length; i++) {
+			
+			
+			//Get the text file
+			File file = new File(tarjeta.getAbsolutePath(),itemsElementsNames[i].toString());
+
+			if (file.isFile() && !file.isHidden()) {
+			//Read text from file
+			text = new StringBuilder();
+
+			try {
+			    BufferedReader br = new BufferedReader(new FileReader(file));
+			    String line;
+
+			    while ((line = br.readLine()) != null) {
+			        text.append(line);
+			        text.append('\n');
+			    }
+			    
+			    //Get name file
+				itemsElementsNames[i] = filelist[i].getName();
+	   			// Get text from file txt
+	   			itemsElementsTexts[i] = text.toString();
+	   			// default false
+	   			itemsElementsChecked[i] = false;
+			 
+			}
+			catch (IOException e) {
+			    //You'll need to add proper error handling here
+			}
+		   }
+
+		}
+
+		Listadapter Adapter = new Listadapter(this,itemsElementsNames, itemsElementsTexts, itemsElementsChecked);
+		listApps.setAdapter(Adapter);
+		
+		//listApps.setOnItemClickListener((OnItemClickListener) this);
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
+		// Menu Configuration and About To
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.main, menu);
+		
+		// Buttons Sync and Archive
+		Button buttonSync = (Button) findViewById(R.id.buttonSync);
+		Button buttonArchive = (Button) findViewById(R.id.buttonArchive);
+		
+		buttonSync.setOnClickListener (new OnClickListener() {
+			public void onClick(View view) {
+				Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+				sync(view);
+			}
+		});
+		
+		buttonArchive.setOnClickListener (new OnClickListener() {
+			public void onClick(View view) {
+				Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+				archive(view);
+			}
+		});
+		
 		return true;
 	}
+	 
 	
 	@Override
 	public boolean onOptionsItemSelected (MenuItem item) {
@@ -100,17 +190,22 @@ public class MainActivity extends ListActivity {
 			Intent refresh = new Intent(this, MainActivity.class);
 		    startActivity(refresh);
 		    this.finish();
-			//tv1 = (TextView) findViewById(R.id.textView1);
-			//tv1.setText(data.getStringExtra("path"));
 		}
 		if (requestCode==STATIC_INTEGER_VALUE_CONFIG_ACTUAL && resultCode== RESULT_OK && data.getStringExtra("path")!="" ){
-			tv1 = (TextView) findViewById(R.id.textView1);
-			tv1.setText(data.getStringExtra("path")); 
+			tvPath = (TextView) findViewById(R.id.textViewRouteConfig);
+			tvPath.setText(data.getStringExtra("path")); 
+			// only for testing path route : to delete
+			tv2 = (TextView) findViewById(R.id.textView2);
+			tv2.setText(data.getStringExtra("path"));
 		}
 	}
 
 	
 	public void sync(View v) {
+		tv2 = (TextView) findViewById(R.id.textView2);
+		tv2.setText("sync: "+((TextView) findViewById(R.id.textViewRouteConfig)).getText().toString());
+		
+		/*
 		tv1 = (TextView) findViewById(R.id.textView1);
 		iv1 = (ImageView) findViewById(R.id.imageButtonSync);
 		String filename = tv1.getText().toString();;
@@ -136,9 +231,13 @@ public class MainActivity extends ListActivity {
 				
 		} catch (IOException e) {
 		}
+		*/
 	}
 	
 	public void archive(View v) {
+		tv2 = (TextView) findViewById(R.id.textView2);
+		tv2.setText("archive: "+((TextView) findViewById(R.id.textViewRouteConfig)).getText().toString());
+		/*
 		tv1 = (TextView) findViewById(R.id.textView1);
 		iv1 = (ImageView) findViewById(R.id.imageButtonArchive);
 		
@@ -159,48 +258,7 @@ public class MainActivity extends ListActivity {
 			//iv1.setImageResource(R.id.imageButtonArchive);
 		} catch (IOException e) {
 		}
-	}
-/**
- 	public void grabar(View v) {
- 
-		String nomarchivo = et1.getText().toString();
-		String contenido = et2.getText().toString();
-		try {
-			File tarjeta = Environment.getExternalStorageDirectory();
-			File file = new File(tarjeta.getAbsolutePath(), nomarchivo);
-			OutputStreamWriter osw = new OutputStreamWriter(
-					new FileOutputStream(file));
-			osw.write(contenido);
-			osw.flush();
-			osw.close();
-			Toast.makeText(this, "Los datos fueron grabados correctamente",
-					Toast.LENGTH_SHORT).show();
-			et1.setText("");
-			et2.setText("");
-		} catch (IOException ioe) {
-		}
+		*/
 	}
 
-	public void recuperar(View v) {
-		String nomarchivo = et1.getText().toString();
-		File tarjeta = Environment.getExternalStorageDirectory();
-		File file = new File(tarjeta.getAbsolutePath(), nomarchivo);
-		try {
-			FileInputStream fIn = new FileInputStream(file);
-			InputStreamReader archivo = new InputStreamReader(fIn);
-			BufferedReader br = new BufferedReader(archivo);
-			String linea = br.readLine();
-			String todo = "";
-			while (linea != null) {
-				todo = todo + linea + "\n";
-				linea = br.readLine();
-			}
-			br.close();
-			archivo.close();
-			et2.setText(todo);
-
-		} catch (IOException e) {
-		}
-	}
-*/
 }
